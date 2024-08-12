d52c6ee8ad560ab95a10b49ca4fb7a743987cf3b
heisenbergO312 <heisenberg.03123@gmail.com>
2024-08-12 20:22:56 +0530

    commit

--- client/src/assets/data/services.js
+++ client/src/assets/data/services.js
@@ -9,53 +9,11 @@ export const services = [
   },
   {
     id:2,
-    name: "Chat Bot",
-    desc: "AI-powered chatbot designed to respond to your mental health-related inquiries. Available 24*7 for you.",
-    bgColor: "rgba(254, 182, 13, .2)",
-    textColor: "#FEB60D",
-    url: 'https://mentalharbor-minor.streamlit.app/'
-  },
-  {
-    id:3,
     name: "Blogs",
     desc: "A comprehensive library of blogs on mental health authored by qualified professionals updated regularly.",
     bgColor: "rgba(254, 182, 13, .2)",
     textColor: "#FEB60D",
     url:'/blog'
   },
-  // {
-  //   id:2,
-  //   name: "Labor & Delivery",
-  //   desc: "World-class care for Labor & Delivery. Our health System offers unmatched, expert health care. From the lab to the clinic.",
-  //   bgColor: "rgba(151, 113, 255, .2)",
-  //   textColor: "#9771FF",
-  // },
-  // {
-  //   id:3,
-  //   name: "Heart & Vascular",
-  //   desc: "Heart care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
-  //   bgColor: "rgba(1, 181, 197, .2)",
-  //   textColor: "#01B5C5",
-  // },
-  // {
-  //   id:4,
-  //   name: "Mental Health",
-  //   desc: "Best Psychatrists for all. Our health System offers unmatched, expert health care. From the lab to the clinic.",
-  //   bgColor: "rgba(1, 181, 197, .2)",
-  //   textColor: "#01B5C5",
-  // },
-  // {
-  //   id:5,
-  //   name: "Neurology",
-  //   desc: "World-class Nurosurgeons. Our health System offers unmatched, expert health care. From the lab to the clinic.",
-  //   bgColor: "rgba(254, 182, 13, .2)",
-  //   textColor: "#FEB60D",
-  // },
-  // {
-  //   id:6,
-  //   name: "Burn Treatment",
-  //   desc: "World-class care for Burns. Our health System offers unmatched, expert health care. From the lab to the clinic.",
-  //   bgColor: "rgba(151, 113, 255, .2)",
-  //   textColor: "#9771FF",
-  // },
+  
 ];
